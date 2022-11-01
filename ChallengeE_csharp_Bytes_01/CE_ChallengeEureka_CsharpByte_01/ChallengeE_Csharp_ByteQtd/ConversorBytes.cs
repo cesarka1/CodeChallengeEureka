5f223bab672cbf92c7ec2b7a97974bc5b097aed4
Escola <105940525+Nitierk@users.noreply.github.com>
2022-11-01 14:27:19 -0300

    Project

--- ChallengeE_csharp_Bytes_01/CE_ChallengeEureka_CsharpByte_01/ChallengeE_Csharp_ByteQtd/ConversorBytes.cs
+++ ChallengeE_csharp_Bytes_01/CE_ChallengeEureka_CsharpByte_01/ChallengeE_Csharp_ByteQtd/ConversorBytes.cs
@@ -8,22 +8,67 @@ namespace ChallengeE_Csharp_ByteQtd
 {
     public class ConversorBytes
     {
-
+        int valor;
+        int count;
         public int getQuantidade(string[] valores)
         {
-           //implemente aqui
-            return 0;
+            
+            //implemente aqui
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                try
+                {
+                    valor = Convert.ToInt32(valores[i]);
+                    if (valor <= 255)
+                    {
+                        count++;
+                    }
+                }
+                catch (Exception)
+                {
+
+                }
+                
+            }
+            return count;
         }
 
         public int getQuantidadeFormato(string[] valores)
         {
-            //implemente aqui
-            return 0;
+            for (int i = 0; i < valores.Length; i++)
+            {
+                try
+                {
+                    valor = Convert.ToInt32(valores[i]);
+                }
+                catch (Exception)
+                {
+                    count++;
+                }
+                
+            }
+            return count;
         }
         public int getQuantidadeForaDaFaixa(string[] valores)
         {
-            //implemente aqui
-            return 0;
+            for (int i = 0; i < valores.Length; i++)
+            {
+                try
+                {
+                    valor = Convert.ToInt32(valores[i]);
+                    if (valor > 255)
+                    {
+                        count++;
+                    }
+                }
+                catch (Exception)
+                {
+                   
+                }
+                
+            }
+            return count;
         }
     }
 }
